72e3baa80ae4d83f9632ac1fb74aafd08e409577
raushan7964 <raushansharma74940@gmail.com>
2026-05-03 16:19:20 +0530

    new deploy on jenkines

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import express from 'express'
 const app = express()
-const port = (3000, '0.0.0.0')
 
 app.get('/', (req, res) => {
   res.send('Hello, World! rauhsan')
 })
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`)
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000')
 })
